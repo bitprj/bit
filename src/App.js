066b1485b016de4126d802eb2b3429851128abbd
wongband <bmwong@ucdavis.edu>
2020-05-10 17:02:59 -0700

    Added more packages for project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
-import logo from './logo.svg';
 import Home from './components/Home'
+import WithGlobalHOC from './components/HOC/WithGlobalHOC'
 
 const App = () => {
 	return (
 		<BrowserRouter>
-              <Switch>
-                  <Route path="/" exact component={Home} />
-              </Switch>
+			<WithGlobalHOC>
+				<Switch>
+					<Route path="/" exact component={Home} />
+				</Switch>
+			</WithGlobalHOC>
 		</BrowserRouter>
 	)
 }
