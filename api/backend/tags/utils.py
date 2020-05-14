24f48beae923e8c75237c435085bf3fe1f245971
xerilius <gcw.chung@gmail.com>
2020-05-14 22:46:31 +0000

    Implemented create_tag function

--- api/backend/tags/utils.py
+++ api/backend/tags/utils.py
@@ -1,4 +1,12 @@
 from ..models import Tag
 
 # Function to create a tag
-def create_tag():
+def create_tag(form_data):
+    tag = Tag(name=form_data["name"],
+              summary=form_data["summary"],
+              submission_guidelines=form_data["submission_guidelines"],
+              about=form_data["about"],
+              emoji=form_data["emoji"]
+            )
+
+    return tag
