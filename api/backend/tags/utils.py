fda6c3bb04ca8190f2218b351a163e1a95a1ea8a
xerilius <gcw.chung@gmail.com>
2020-05-15 00:42:20 +0000

    Implemented Update Tag function

--- api/backend/tags/utils.py
+++ api/backend/tags/utils.py
@@ -1,5 +1,6 @@
 from ..models import Tag
 
+
 # Function to create a tag
 def create_tag(form_data):
     tag = Tag(name=form_data["name"],
@@ -10,3 +11,14 @@ def create_tag(form_data):
             )
 
     return tag
+
+
+# Function to update a tag
+def update_tag(tag, form_data):
+  tag.name = form_data["name"]
+  tag.summary = form_data["name"]
+  tag.submission_guidelines = form_data["submission_guidelines"]
+  tag.about = form_data["about"]
+  tag.emoji = form_data["emoji"]
+
+  return 
