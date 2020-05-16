330c8c581276f51b1241f0b60f435e1428606435
wongband <bmwong@ucdavis.edu>
2020-05-16 01:33:31 -0700

    Added font family and changed file name to be more generic

---
+++ src/config/fonts.js
@@ -0,0 +1,21 @@
+import '../assets/fonts/Calibre/Calibre-Regular.ttf'
+import '../assets/fonts/SFMono/SFMono-Regular.ttf'
+
+const fonts = {
+    family: {
+        calibre: 'Calibre-Regular',
+        sfMono: 'SFMono-Regular'
+    },
+    sizes: {
+        xs: '12px',
+        smish: '13px',
+        sm: '14px',
+        md: '16px',
+        lg: '18px',
+        xl: '20px',
+        xxl: '22px',
+        h3: '32px'
+    }
+}
+
+export default fonts
