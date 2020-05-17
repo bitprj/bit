17042f822364dd5c33cf04afd62a2837296f3f7e
zexing <zexingfa@usc.edu>
2020-05-16 22:20:22 -0700

    Changed Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import ToggleTab from'./shared/Tab/ToggleTab'
 const Home = () => {
     return( 
     <>
-        <FollowerList followers = {[1,2,3,4]}/>
+        <FollowerList followers = {[{"buttonTxt" : "+ Follow"}, {"buttonTxt" : "+ Follow"},{"buttonTxt" : "Following"},{"buttonTxt" : "+ Follow"}]}/>
         <ToggleTab tabs = {['Posts(24)', 'Followers(4)', 'Following(12)']} activeNum ={1}></ToggleTab>
         <ToggleTab tabs = {['Posts(24)', 'Followers(4)', 'Following(12)']} vertical = {true} activeNum ={0}></ToggleTab>
     </>
